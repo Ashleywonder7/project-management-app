26a7f41f3d8327998d415c65761f76155d78efc7
Barima <ashleybarima17@gmail.com>
2026-09-04 16:33:15 +0000

    Connect frontend to live authenticated API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './ConnectedApp';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
